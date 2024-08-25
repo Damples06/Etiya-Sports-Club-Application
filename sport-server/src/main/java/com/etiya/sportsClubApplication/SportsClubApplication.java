3991288ad2ed6f87a0726317149ad3507aef6895
Selçuk Yılmaz <selcuk_yilmaz06@outlook.com>
2024-08-25 19:32:23 +0300

    Final version of the project at the end of my internship at Etiya. Will continue development based on future needs.

--- sport-server/src/main/java/com/etiya/sportsClubApplication/SportsClubApplication.java
+++ sport-server/src/main/java/com/etiya/sportsClubApplication/SportsClubApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SportsClubApplication {
-//Todo: Maintain dto package and its classes.
 	public static void main(String[] args) {
 		SpringApplication.run(SportsClubApplication.class, args);
 	}
